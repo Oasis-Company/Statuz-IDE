d991c0baa5a29e84697b8f4728fdece74cf9488c
CeaserZhao <chkevin.zhao@gmail.com>
2026-07-17 16:00:43 +0800

    chore(statuz): commit src/vs/workbench/contrib/issue/browser/baseIssueReporterService.ts

--- src/vs/workbench/contrib/issue/browser/baseIssueReporterService.ts
+++ src/vs/workbench/contrib/issue/browser/baseIssueReporterService.ts
@@ -875,7 +875,7 @@ export class BaseIssueReporterService extends Disposable {
 
 		sourceSelect.innerText = '';
 		sourceSelect.append(this.makeOption('', localize('selectSource', "Select source"), true));
-		sourceSelect.append(this.makeOption(IssueSource.VSCode, localize('vscode', "Visual Studio Code"), false));
+		sourceSelect.append(this.makeOption(IssueSource.VSCode, localize('vscode', "Statuz IDE"), false));
 		sourceSelect.append(this.makeOption(IssueSource.Extension, localize('extension', "A VS Code extension"), false));
 		if (this.product.reportMarketplaceIssueUrl) {
 			sourceSelect.append(this.makeOption(IssueSource.Marketplace, localize('marketplace', "Extensions Marketplace"), false));
